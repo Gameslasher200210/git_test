3f156ee79fa060116b98d5fbb72e815312b8ed3b
TKpc <mmukhethwa8@gmail.com>
2023-07-19 17:06:51 +0200

    used a scanner

--- main.java
+++ main.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Scanner a = new Scanner(System.in);
+        System.out.println("input c");
+        int b = a.nextInt();
+        System.out.println(b);
+       a.close();
     }
 }
